997e11212e0bc8e1f22a28b9a4c2615b244cb2c6
DESKTOP-RQQ5IUJ\SpikenMC <60916@gymnasium.ax>
2024-10-07 10:31:52 +0300

    JAg är färdig med appen

--- ToDo/src/App.vue
+++ ToDo/src/App.vue
@@ -1,17 +1,18 @@
 <script setup>
 import { ref } from 'vue'
 
-const list = ref([
-  { text: 'Köp mjölk', done: false },
-  { text: 'Köp ägg', done: true },
-  { text: 'Köp bröd', done: false }
-])
+const list = ref([])
 const itemText = ref('')
 
 function addItem() {
   let listItem = { text: itemText.value, done: false }
   list.value.push(listItem)
   itemText.value = ''
+}
+function radera(listItem) {
+  list.value = list.value.filter((obj)=> {
+    return obj !== listItem 
+  })
 }
 </script>
 
@@ -23,15 +24,63 @@ function addItem() {
   </header>
 
   <main>
-    <h3>Att göra:</h3>
+    <h3 id="todo">Att göra:</h3>
     <ul>
       <template v-for="itm in list" v-bind:key="itm">
-        <li>
-          {{ itm.text }}
+        <li v-if="!itm.done" class="todoItm" @click="itm.done = !itm.done">
+          {{ itm.text }}<span title="Radera" @click="radera(itm)">X</span>
+        </li>
+      </template>
+    </ul>
+    <h3 id="klart">Klart:</h3>
+    <ul>
+      <template v-for="itm in list"  v-bind:key="itm">
+        <li v-if="itm.done" class="todoItm done" @click="itm.done = !itm.done">
+          {{ itm.text }}<span title="Radera" @click="radera(itm)">X</span>
         </li>
       </template>
     </ul>
   </main>
 </template>
 
-<style scoped></style>
+<style scoped>
+header {
+  color: purple;
+}
+  body {
+    background-color: rgb(221, 73, 221);
+  }
+.todoItm {
+  font-weight: bold;
+  padding: 0.5em;
+  list-style-type: none;
+  cursor: pointer;
+}
+.todoItm:nth-child(odd) {
+  background-color: green;
+  color: black;
+}
+.todoItm:nth-child(even) {
+  background-color:purple;
+  color: black;
+}
+.done {
+  text-decoration: line-through solid black;
+}
+#klart {
+  color: purple;
+}
+#todo {
+  color: green;
+}
+li span {
+  display: inline-block;
+  position: absolute;
+  right: 3em;
+  background-color: rgb(224, 13, 224) ;
+  color: wheat;
+  padding-left: 0.5em;
+  padding-right: 0.5em;
+}
+</style>
+
