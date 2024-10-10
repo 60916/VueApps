5a6472d24163a18a194bc7c87fdb98582d39fa6a
DESKTOP-RQQ5IUJ\SpikenMC <60916@gymnasium.ax>
2024-10-10 13:33:58 +0300

    Gjort en färdig app

--- ssp/vite.config.js
+++ ssp/vite.config.js
@@ -5,9 +5,8 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  base: '/~60916/ssp',
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
